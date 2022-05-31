25e4e39a87bf81dfb7464a62082ea9737ccc21fc
kimeunji94 <kej01048535793@gmail.com>
2022-05-31 13:35:37 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/book/freetecspringboot2webservice/FreetecSpringboot2WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freetecspringboot2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreetecSpringboot2WebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreetecSpringboot2WebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jojoldu/book/freetecspringboot2webservice/FreetecSpringboot2WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.freetecspringboot2webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreetecSpringboot2WebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
